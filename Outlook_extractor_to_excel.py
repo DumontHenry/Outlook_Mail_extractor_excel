51a2423404c5f04f604b82e086e6afedb190a85f
Henry-Philippe Dumont <67460798+DumontHenry@users.noreply.github.com>
2021-05-06 21:43:15 +0200

    Update Outlook_extractor_to_excel.py

--- Outlook_extractor_to_excel.py
+++ Outlook_extractor_to_excel.py
@@ -16,7 +16,8 @@ def outlook_email():
     inbox=mapi.GetDefaultFolder(6) # the number is equal to the file name type , check windows to understand 
     # inbox=mapi.GetDefaultFolder(6).Folders["your_sub_folder"]
     message= inbox.Items
-
+    #validated_mails=mapi.GetDefaultFolder(6).Folders("Done")
+    
     #dataframes
     df = pd.DataFrame(columns = ['ReceiveDate', 'Subject', 'Sender', 'Importance', "Body"])
 
@@ -24,20 +25,21 @@ def outlook_email():
 
     count = 0
     for messageDetails in message:
-        if messageDetails.UnRead==True:
+        if messageDetails.UnRead==True and messageDetails.SenderEmailAddress.startswith('xxxxxx') and messageDetails.Subject.startswith('xxxxx'):
             df.loc[count,"ReceiveDate"] = (messageDetails.ReceivedTime).strftime("%m/%d/%Y, %H:%M:%S")
             df.loc[count,"Subject"] = (messageDetails.Subject)
             df.loc[count,"Sender"] = (messageDetails.SenderEmailAddress)
             df.loc[count,"Importance"] = (messageDetails.Importance)
             df.loc[count,"Body"] = (messageDetails.Body)
             messageDetails.UnRead =False
+            #messageDetails.Move(validated_mails)
             count += 1
 
     print('loading.....')
     print(df.head(5))
 
-    book = load_workbook("xxxxx.xlsx")
-    writer = pd.ExcelWriter('xxxxxx.xlsx', engine='openpyxl', mode="a")
+    book = load_workbook("C:/tetx1.xlsx")
+    writer = pd.ExcelWriter('C:/tetx1.xlsx', engine='openpyxl', mode="a")
     writer.book = book
     writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
